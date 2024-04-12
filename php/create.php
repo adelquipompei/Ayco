eeb4d7c3aa2effa5cba710fb1a1dd7758d023509
keko <adelquipompei@gmail.com>
2024-04-12 01:11:47 -0300

    aaaaa

--- php/create.php
+++ php/create.php
@@ -2,22 +2,22 @@
 
 include("conexion.php");
 
-if ($conn->connect_error) { 
-    die('no'. $conn->connect_error);
-}else{
-    echo'Conexion establecida!';
+if ($conn->connect_error) {
+    die('no' . $conn->connect_error);
+} else {
+    echo 'Conexion establecida!';
 }
 
 $auto = array(
     'marca' =>  $_POST['marca'],
-    'modelo'=> $_POST['modelo'],
-    'img_name'=> $_FILES['img']['name'],
-    'file'=> $_FILES['img']['tmp_name'],
-    'year'=> $_POST['year']
+    'modelo' => $_POST['modelo'],
+    'img_name' => $_FILES['img']['name'],
+    'file' => $_FILES['img']['tmp_name'],
+    'year' => $_POST['year']
 );
 
-$ruta = "img/uploaded_images/".$auto['img_name'];
-move_uploaded_file($auto['file'], "../".$ruta);
+$ruta = "img/uploaded_images/" . $auto['img_name'];
+move_uploaded_file($auto['file'], "../" . $ruta);
 
 echo '<pre>';
 
@@ -26,33 +26,18 @@ print_r($auto);
 echo '</pre>';
 
 
-$sql = "INSERT INTO cars (marca,modelo,imagen,año) VALUES ('" . $auto['marca'] . "' , '" . $auto['modelo'] . "' , '".$ruta."', '".$auto['year']."')";
+$sql = "INSERT INTO cars (marca,modelo,imagen,año) VALUES ('" . $auto['marca'] . "' , '" . $auto['modelo'] . "' , '" . $ruta . "', '" . $auto['year'] . "')";
 
 
 
 $result = $conn->query($sql);
 
-if($result){
-    echo"<br>";
-    echo'Contenido insertado!';
+if ($result) {
+    echo "<br>";
+    echo 'Contenido insertado!';
 }
 
 
 
 
 $conn->close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-?>
